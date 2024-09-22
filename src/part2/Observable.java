4144f3de23b75b68d0eb3e6576a05fddbee12ede
Muhammad Abdullah <ma3472995@gmail.com>
2024-09-22 21:04:01 +0500

    Observer implmented

--- src/part2/Observable.java
+++ src/part2/Observable.java
@@ -1,5 +1,19 @@
 package part2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Observable {
+	
+	private List<Observer> observers = new ArrayList<>();
+	
+	public void addObserver(Observer ob) {
+		observers.add(ob);
+	}
+	public void notifyObservers() {
+		for (Observer ob: observers) {
+			ob.update();
+		}
+	}
 
 }
--- src/part2/Observer.java
+++ src/part2/Observer.java
@@ -1,5 +1,6 @@
 package part2;
 
 public interface Observer {
+	void update();
 
 }
